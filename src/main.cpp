bf174aa0779050c90ab1eaca2d4093727288ac28
shahrad <shahradArianNezhad@gmail.com>
2026-07-02 10:53:00 +0330

    added a constexpr to change modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "shadowDelve/shadowDelve.hpp"
 #include "mapEditor/mapEditor.hpp"
 
+constexpr bool editorMode = false;
 
 int main(){
-  ShadowDelve game;
-  //MapEditor game;
-  game.run();
-  return 0;
+  if(editorMode){
+    MapEditor game;
+    game.run();
+    return 0;
+  }else{
+    ShadowDelve game;
+    game.run();
+    return 0;
+  }
 }
